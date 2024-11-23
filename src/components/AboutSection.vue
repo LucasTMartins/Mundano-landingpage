a1d4a40099e7e524eb5e054f345e7a7704288cd1
Lucas Martins de Oliveira <lucasm236954@gmail.com>
2024-11-22 21:01:03 -0300

    Creating start-page

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -3,7 +3,6 @@
     <div class="container text-center">
         <h2>#1 App para Planejamento de Mundos e Histórias</h2>
         <p class="lead">Mundano é um software inovador projetado especialmente para escritores que desejam dar vida a universos ricos e detalhados. Com ferramentas intuitivas para criação de mapas, desenvolvimento de personagens, e construção de histórias, ele permite que você organize suas ideias de forma visual e interativa. Explore diferentes culturas, crie mitologias envolventes e conecte tramas complexas, tudo em um único lugar. Transforme sua imaginação em realidade e leve suas narrativas a um novo patamar!.</p>
-      <!-- Adicione mais conteúdo ou componentes, conforme necessário -->
     </div>
   </section>
 </template>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <h1 class="display-4">O Software Definitivo para Escritores Criativos</h1>
       <p class="lead">Construindo, organizando e gerenciando mundos fictícios para qualquer propósito.</p>
-      <div>
+      <div class="flex; flex-direction-row;">
         <button class="btn btn-primary mx-2">Ler Mais</button>
         <button class="btn btn-secondary mx-2">Código Fonte</button>
+        <img src="/Laptop_Mundano.png" alt="Laptop Image with Mundano Software on screen" class="mt-4 height-2">
       </div>
-      <!-- <img src="phone-image.png" alt="Phone Image" class="mt-4"> -->
     </div>
   </section>
 </template>
